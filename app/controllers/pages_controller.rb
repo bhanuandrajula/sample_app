61ebe81ad973ea3834a28d6555e3d3610ddac45b
bhanuandrajula <bhanua1985@gmail.com>
2014-08-12 15:42:36 -0400

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home"
   end
 
   def contact
+  @title = "Bhanu"
+  end
+  
+  def about
+  @title = "about"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe PagesController, :type => :controller do
+ render_views
 
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to be_success
     end
-  end
+	  it "should have a non blank body" do
+      get :home
+      expect(response).not_to match(/<body>\s*<\/body>/)
+    end
 
+  end
+  
+  
   describe "GET contact" do
     it "returns http success" do
       get :contact
       expect(response).to be_success
     end
   end
-
+  describe "GET about" do
+    it "returns http success" do
+      get :about
+      expect(response).to be_success
+    end
+  end
 end
