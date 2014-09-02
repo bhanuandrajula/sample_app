88b19f8f1e20f08b1e0880ffb14f1c66274853cb
bhanuandrajula <bhanua1985@gmail.com>
2014-09-02 16:47:32 -0400

    Micropost validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Micropost, :type => :model do
   describe Micropost do
-	# before(:each) do
-		# @user_attr = {:name => "Sriniuser",:email=> "Sriniuser@gmail.com", :password => "srini@123" , :password_confirmation => "srini@123"}
-		# @user = User.create!(@user_attr)
-		# @attr = {:content => "Bhanu's microposts"}
-	# end
+	before(:each) do
+		@user_attr = {:name => "Sriniuser",:email=> "Sriniuser@gmail.com", :password => "srini@123" , :password_confirmation => "srini@123"}
+		@user = User.create!(@user_attr)
+		@attr = {:content => "Bhanu's microposts"}
+	end
 	 let(:user) { FactoryGirl.create(:user) }
 	  before do
 		# This code is not idiomatically correct.
@@ -18,6 +18,35 @@ RSpec.describe Micropost, :type => :model do
 	# end
 		it { should respond_to(:content) }
 		it { should respond_to(:user_id) }
+  end
+  
+  describe "Validations" do
+  
+  	# before(:each) do
+		# @user_attr = {:name => "Sriniuser",:email=> "Sriniuser@gmail.com", :password => "srini@123" , :password_confirmation => "srini@123"}
+		# @user = User.create!(@user_attr)
+		# @attr = {:content => "Bhanu's microposts", :user_id => @user.id}
+	# end
+	  before do
+		# This code is not idiomatically correct.
+		@micropost = Micropost.new(content: "Bhanu's microposts", user_id: user.id)
+	  end
+  
+	   # describe "when user_id is not present" do
+		 # before { @micropost.user_id = nil }
+		 # it { should_not be_valid }
+		# end
+
+	  # describe "with blank content" do
+		# before { @micropost.content = " " }
+		# it { should_not be_valid }
+	  # end
+
+	  # describe "with content that is too long" do
+		# before { @micropost.content = "a" * 141 }
+		# it { should_not be_valid }
+	  # end
+	
   end
 end
 
