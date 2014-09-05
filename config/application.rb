95645843d8ca7893f39e2db29cf76aed3af3d6f4
bhanuandrajula <bhanua1985@gmail.com>
2014-09-05 16:24:42 -0400

    Added files

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,6 +23,9 @@ module SampleApp
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+	
+	# Heroku requires this to be false
+	config.assets.initialize_on_precompile=false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
